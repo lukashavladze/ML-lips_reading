275a7bee5ca5c88c8c2d81e7a96daf11b3ea55cc
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2025-02-20 18:32:51 +0400

    added video preprocessing

--- data_loading_functions_cv2.py
+++ data_loading_functions_cv2.py
@@ -8,14 +8,7 @@ from matplotlib import pyplot as plt
 import imageio
 import os
 
-# for downloading data
 
-# url = "https://drive.google.com/uc?id=1YlvpDLix3S-U8fd-gqRwPcWXAXm8JwjL"
-# output = 'data.zip'
-# gdown.download(url, output, quiet=False)
-# gdown.extractall('data.zip')
-
-# my added function
 def preprocess_frame(frame):
     """ Preprocess the frame to remove dense regions, normalize shades, and enhance visibility. """
 
@@ -56,38 +49,14 @@ def load_video_mpg(path: str) -> np.ndarray:
 
     return frames_array
 
-
-
-
-def load_video(path:str) -> List[float]:
-
-    cap = cv2.VideoCapture(path)
-    frames = []
-    # capturing frames from location: 190:236 ...
-    for _ in range(int(cap.get(cv2.CAP_PROP_FRAME_COUNT))):
-        ret, frame = cap.read()
-        frame = tf.image.rgb_to_grayscale(frame)
-        frames.append(frame[190:236, 80:220, :])
-    cap.release()
-
-    mean = tf.math.reduce_mean(frames)
-    std = tf.math.reduce_std(tf.cast(frames, tf.float32))
-    return tf.cast((frames - mean), tf.float32) / std
-
 # vocabulary for text generation
 vocab = [x for x in "abcdefghijklmnopqrstuvwxyz'?!123456789 "]
 vocab_georgian = [x for x in "აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ'?!123456789 "]
 
-#function for converting numbers into chars and chars to num
-# oov_token = if no char then returns empty string
 
 char_to_num = keras.src.layers.StringLookup(vocabulary=vocab, oov_token="")
 num_to_char = keras.src.layers.StringLookup(vocabulary=char_to_num.get_vocabulary(), oov_token="", invert=True)
 
-#print(f"the vocabulary is: {char_to_num.get_vocabulary()}"
-#      f"(size ={char_to_num.vocabulary_size()}")
-
-#print(char_to_num(["a", "b"]))
 
 def load_alignments(path:str) -> List[str]:
     with open(path, 'r') as f:
@@ -105,13 +74,17 @@ def load_data(path: str):
     file_name = path.split('\\')[-1].split('.')[0]
     video_path = os.path.join('data', 's1', f"{file_name}.mpg")
     alignment_path = os.path.join('data', 'alignments', 's1', f"{file_name}.align")
-    frames = load_video(video_path)
-    #frames = load_video_mpg(video_path)
+    frames = load_video_mpg(video_path)
     alignments = load_alignments(alignment_path)
 
     return frames, alignments
 
+
 test_path = ".\\data\\s1\\bbal6n.mpg"
+
+frames_array = load_video_mpg(test_path)
+frames_uint8 = (frames_array.squeeze() * 255).astype(np.uint8)
+#imageio.mimsave('./cv2_animation_new.gif', frames_uint8, fps=10)
 
 # getting file name from data
 tf.convert_to_tensor(test_path).numpy().decode('utf-8').split("\\")[-1].split('.')[0]
@@ -134,18 +107,16 @@ data = data.map(mappable_function)
 data = data.padded_batch(2, padded_shapes=([75, None, None, None], [40]))
 data = data.prefetch(tf.data.AUTOTUNE)
 
-frames, alignments = data.as_numpy_iterator().next()
-
 test = data.as_numpy_iterator()
 val = test.next()
 
-# transforms numpy array into animation
-frames_to_save = np.squeeze(val[0][1])
+frames_to_save = np.squeeze(val[0][0])
 frames_to_save = (frames_to_save * 255).astype(np.uint8)
 
-imageio.mimsave('./animation_[0, 1].gif', frames_to_save, fps=10)
+imageio.mimsave('./animation_0_new.gif', frames_to_save, fps=10)
 
-print(tf.strings.reduce_join([num_to_char(word) for word in frames_to_save[0][1]]))
+
+
 
 
 
