3941abc0eb2eb2c23e2a65d0cfa29d0fd92ce1bb
LSHAVLADZE <LSHAVLADZE@WS002HP290G9>
2025-02-25 11:36:28 +0400

    update

--- setup_training_options_and_Train.py
+++ setup_training_options_and_Train.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+import data_loading_functions
 
 def scheduler(epoch, lr):
     if epoch < 30:
@@ -13,6 +15,21 @@ def CTCLoss(y_true, y_pred):
 
     input_length = input_length * tf.ones(shape=(batch_len, 1), dtype="int64")
     label_length = label_length * tf.ones(shape=(batch_len, 1), dtype="int64")
+
     # input length should be 75, label_length = 40 in our case
     loss = tf.keras.backend.ctc_batch_cost(y_true, y_pred, input_length, label_length)
     return loss
+
+class ProduceExample(tf.keras.callbacks.Callback):
+    def __init__(self, dataset) -> None:
+        self.dataset = dataset.as_numpy_iterator()
+
+    def on_epoch_end(self, epoch, logs=None) -> None:
+        data = self.dataset.next()
+        yhat = self.model.predict(data[0])
+        decoded = tf.keras.backend.ctc_decode(yhat, [75, 75], greedy=False)[0][0].numpy()
+        for x in range(len(yhat)):
+            print('Original:', tf.strings.reduce_join([data_loading_functions.vocab[word] + '' for word in data[1][x]]).numpy().decode('utf-8'))
+            print('Prediction:', tf.strings.reduce_join([data_loading_functions.vocab[word] + '' for word in decoded[x]]).numpy().decode('utf-8'))
+            print('-' * 100)
+
